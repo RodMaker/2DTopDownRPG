423baec61db611601893cf3e01cc6c7eb112d5a8
RodMaker <83619630+RodMaker@users.noreply.github.com>
2024-01-08 00:06:21 +0000

    Part 36 + Leaf Particle System

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -41,6 +41,7 @@ public class PlayerHealth : MonoBehaviour
             return;
         }
 
+        ScreenShakeManager.Instance.ShakeScreen();
         knockback.GetKnockedBack(hitTransform, knockBackThrustAmount);
         StartCoroutine(flash.FlashRoutine());
         canTakeDamage = false;
